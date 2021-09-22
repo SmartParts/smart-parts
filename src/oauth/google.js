56a1bdaff666a28312f977d5b7c6b437a53e0e1f
Psyphon361 <tanish03jun@gmail.com>
2021-09-22 15:22:55 +0530

    Edit Google OAuth

--- src/oauth/google.js
+++ src/oauth/google.js
@@ -17,7 +17,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "http://localhost:3000/google/callback",
+            callbackURL: "https://smart-parts.herokuapp.com/google/callback",
         },
         async function (accessToken, refreshToken, profile, cb) {
             User.findOrCreate(
